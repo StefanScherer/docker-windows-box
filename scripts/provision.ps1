b6f4e6e943f68c090eed5475fab33c194b9afa2b
Stefan Scherer <scherer_stefan@icloud.com>
2015-05-31 19:01:19 +0200

    use docker-machine chocolatey package

--- scripts/provision.ps1
+++ scripts/provision.ps1
@@ -1,12 +1,11 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install -y curl
 choco install -y virtualbox
 choco install -y docker
-
-curl -k -L -o C:\programdata\chocolatey\bin\docker-machine.exe https://github.com/docker/machine/releases/download/v0.2.0-rc3/docker-machine_windows-amd64.exe
+choco install -y docker-machine
 
 setx CYGWIN nodosfilewarning
+$ENV:CYGWIN="nodosfilewarning"
 
 docker-machine create -d virtualbox dev
 docker-machine env dev
