cd2383f2024118b30106ed672b360043fcf64daa
Stefan Scherer <scherer_stefan@icloud.com>
2015-11-28 21:07:35 +0100

    Tag base images if latest tag is lost

--- scripts/update-nightly-docker.ps1
+++ scripts/update-nightly-docker.ps1
@@ -10,4 +10,10 @@ if (!(Test-Path C:\windows\system32\docker-orig.exe)) {
 wget https://master.dockerproject.org/windows/amd64/docker.exe -outfile C:\windows\system32\docker.exe
 "Starting docker service"
 start-service docker
+if ("$(docker images windowsservercore:latest | wc -l)" -eq "1") {
+  docker tag windowsservercore:10.0.10586.0 windowsservercore:latest
+}
+if ("$(docker images nanoserver:latest | wc -l)" -eq "1") {
+  docker tag nanoserver:10.0.10586.0 nanoserver:latest
+}
 docker version
