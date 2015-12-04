8a2a1b52808d651860032fd1953c670d28421bbc
Stefan Scherer <scherer_stefan@icloud.com>
2015-12-04 22:32:41 +0100

    Insert .gitconfig if available

--- scripts/insert-ssh-key.ps1
+++ scripts/insert-ssh-key.ps1
@@ -1,5 +1,13 @@
 if (Test-Path C:\vagrant\resources\.ssh) {
-  Write-Host "Inserting SSH config and keys into userprofile"
-  mkdir $env:USERPROFILE\.ssh
-  copy C:\vagrant\resources\.ssh\* $env:USERPROFILE\.ssh
+  if (!(Test-Path "$env:USERPROFILE\.ssh")) {
+    Write-Host "Inserting SSH config and keys into userprofile"
+    mkdir $env:USERPROFILE\.ssh
+    copy C:\vagrant\resources\.ssh\* $env:USERPROFILE\.ssh
+  }
+}
+
+if (Test-Path C:\vagrant\resources\.gitconfig) {
+  if (!(Test-Path "$env:USERPROFILE\.gitconfig")) {
+    copy C:\vagrant\resources\.gitconfig $env:USERPROFILE\.gitconfig
+  }
 }
