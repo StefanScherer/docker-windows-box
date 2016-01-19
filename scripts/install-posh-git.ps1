cd71cc9e573781e841df2aaf9b159e3cc3fc8594
Stefan Scherer <scherer_stefan@icloud.com>
2016-01-20 00:27:16 +0100

    Improve posh-git installtion

--- scripts/install-posh-git.ps1
+++ scripts/install-posh-git.ps1
@@ -1,8 +1,15 @@
 # install git tab completion (https://github.com/dahlbyk/posh-git)
 #Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 #Install-Module posh-git -Scope CurrentUser
-Push-Location Documents
+
+cd Documents
 git clone https://github.com/dahlbyk/posh-git
 cd posh-git
 . .\install.ps1
-Pop-Location
+
+# patch profile to make it work from winrm session, see PR https://github.com/dahlbyk/posh-git/pull/238
+(Get-Content profile.example.ps1) |
+Foreach-Object {$_ -replace '^Start-SshAgent -Quiet','If ((Test-Path env:\SESSIONNAME) -And ($env:SESSIONNAME -eq "Console")) {
+  Start-SshAgent -Quiet
+}'}  |
+Out-File profile.example.ps1
