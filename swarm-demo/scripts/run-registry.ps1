e8169c70b2bbeb774f77fef59e8d78233d6e9279
Stefan Scherer <scherer_stefan@icloud.com>
2016-05-20 10:48:56 +0200

    Update registry 2.4.1

--- swarm-demo/scripts/run-registry.ps1
+++ swarm-demo/scripts/run-registry.ps1
@@ -1,2 +1,2 @@
 mkdir -p C:\registry-v2
-docker run -d -p 5000:5000 -v "C:\registry-v2:C:\registry" stefanscherer/registry-windows:2.4.1-rc.2
+docker run -d -p 5000:5000 -v "C:\registry-v2:C:\registry" stefanscherer/registry-windows:2.4.1
