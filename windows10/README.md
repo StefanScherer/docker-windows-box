c4900e58fb8e2c60e3525c5884d68e7202c53457
Stefan Scherer <scherer_stefan@icloud.com>
2016-05-29 01:35:50 +0200

    Add scripts for Windows 10 Insider 14352

---
+++ windows10/README.md
@@ -0,0 +1,11 @@
+# Docker on Windows 10 Insider 14352
+
+This `Vagrantfile` installs Docker on Windows 10 Insider 14352 or newer
+as described in https://msdn.microsoft.com/de-de/virtualization/windowscontainers/quick_start/quick_start_windows_10
+
+You need a Vagrant box with Windows 10 Insider 14352.
+
+```
+vagrant up
+```
+
---
+++ windows10/scripts/install-docker.ps1
@@ -0,0 +1,7 @@
+# https://msdn.microsoft.com/de-de/virtualization/windowscontainers/quick_start/quick_start_windows_10
+New-Item -Type Directory -Path 'C:\Program Files\docker\'
+Invoke-WebRequest https://aka.ms/tp5/b/dockerd -OutFile $env:ProgramFiles\docker\dockerd.exe
+Invoke-WebRequest https://aka.ms/tp5/b/docker -OutFile $env:ProgramFiles\docker\docker.exe
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Docker", [EnvironmentVariableTarget]::Machine)
+. 'C:\Program Files\docker\dockerd' --register-service
+Start-Service Docker
