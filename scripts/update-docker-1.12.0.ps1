03c13cd01c4099d87bcf9f8961571b2bd6d922f7
Stefan Scherer <scherer_stefan@icloud.com>
2016-08-03 16:41:38 +0200

    Fix link

--- scripts/update-docker-1.12.0.ps1
+++ scripts/update-docker-1.12.0.ps1
@@ -1,5 +1,5 @@
 stop-service docker
-wget -outfile $env:TEMP\docker-1.12.0.zip https://test.docker.com/builds/Windows/x86_64/docker-1.12.0.zip
+wget -outfile $env:TEMP\docker-1.12.0.zip https://get.docker.com/builds/Windows/x86_64/docker-1.12.0.zip
 Expand-Archive -Path $env:TEMP\docker-1.12.0.zip -DestinationPath $env:TEMP -Force
 copy $env:TEMP\docker\*.exe $env:ProgramFiles\docker
 
