6516ddd2fe1a4b61cdab67fe3f2fc64581c3b722
Stefan Scherer <scherer_stefan@icloud.com>
2016-08-19 11:04:08 +0200

    Update Docker 1.13.0-dev to fix nanoserver issue
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- windows10/scripts/install-docker.ps1
+++ windows10/scripts/install-docker.ps1
@@ -4,10 +4,9 @@ Set-ExecutionPolicy Bypass -scope Process
 Set-ItemProperty -Path 'HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Virtualization\Containers' -Name VSmbDisableOplocks -Type DWord -Value 1 -Force
 
 New-Item -Type Directory -Path "$($env:ProgramFiles)\docker"
-wget -outfile $env:TEMP\docker-1.12.0.zip https://get.docker.com/builds/Windows/x86_64/docker-1.12.0.zip
-Expand-Archive -Path $env:TEMP\docker-1.12.0.zip -DestinationPath $env:TEMP -Force
-copy $env:TEMP\docker\*.exe $env:ProgramFiles\docker
-Remove-Item $env:TEMP\docker-1.12.0.zip
+Invoke-WebRequest "https://master.dockerproject.com/windows/amd64/docker-1.13.0-dev.zip" -OutFile "$env:TEMP\docker-1.13.0-dev.zip" -UseBasicParsing
+Expand-Archive -Path $env:TEMP\docker-1.13.0-dev.zip -DestinationPath $env:ProgramFiles -Force
+Remove-Item $env:TEMP\docker-1.13.0-dev.zip
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$($env:ProgramFiles)\docker", [EnvironmentVariableTarget]::Machine)
 $env:Path = $env:Path + ";$($env:ProgramFiles)\docker"
 . dockerd --register-service -H npipe:// -H 0.0.0.0:2375 -G docker
