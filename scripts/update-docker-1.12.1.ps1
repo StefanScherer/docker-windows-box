bea002f59ec57522c5ea51708998348a86ddb519
Stefan Scherer <scherer_stefan@icloud.com>
2016-09-21 10:10:50 +0200

    Update Docker 1.12.1

--- scripts/update-docker-1.12.1.ps1
+++ scripts/update-docker-1.12.1.ps1
@@ -1,6 +1,6 @@
 stop-service docker
-wget -outfile $env:TEMP\docker-1.12.0.zip https://get.docker.com/builds/Windows/x86_64/docker-1.12.0.zip
-Expand-Archive -Path $env:TEMP\docker-1.12.0.zip -DestinationPath $env:TEMP -Force
+wget -outfile $env:TEMP\docker-1.12.1.zip https://get.docker.com/builds/Windows/x86_64/docker-1.12.1.zip
+Expand-Archive -Path $env:TEMP\docker-1.12.1.zip -DestinationPath $env:TEMP -Force
 copy $env:TEMP\docker\*.exe $env:ProgramFiles\docker
 
 if (Test-Path "$($env:ProgramData)\docker\image\Windows filter storage driver") {
