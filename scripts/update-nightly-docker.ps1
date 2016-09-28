a4ebe8a5deff7522e458c89081c90021c0a6b7a7
Stefan Scherer <scherer_stefan@icloud.com>
2016-09-28 18:02:13 +0200

    Faster download

--- scripts/update-nightly-docker.ps1
+++ scripts/update-nightly-docker.ps1
@@ -1,6 +1,7 @@
 stop-service docker
-wget -outfile $env:ProgramFiles\docker\dockerd.exe -uri https://master.dockerproject.org/windows/amd64/dockerd.exe
-wget -outfile $env:ProgramFiles\docker\docker.exe -uri https://master.dockerproject.org/windows/amd64/docker.exe
+$wc = New-Object net.webclient
+$wc.Downloadfile("https://master.dockerproject.org/windows/amd64/dockerd.exe", "$env:ProgramFiles\docker\dockerd.exe")
+$wc.Downloadfile("https://master.dockerproject.org/windows/amd64/docker.exe", "$env:ProgramFiles\docker\docker.exe")
 
 if (Test-Path "$($env:ProgramData)\docker\image\Windows filter storage driver") {
   ren "$($env:ProgramData)\docker\image\Windows filter storage driver" "$($env:ProgramData)\docker\image\windowsfilter"
