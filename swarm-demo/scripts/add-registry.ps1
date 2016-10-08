8444ab03e9e59d4e25b30dff7ccfdef4b67f18a2
Stefan Scherer <scherer_stefan@icloud.com>
2016-10-08 19:21:50 +0200

    Update to latest windows_2016_docker box

--- swarm-demo/scripts/add-registry.ps1
+++ swarm-demo/scripts/add-registry.ps1
@@ -4,8 +4,9 @@ $ip = "192.168.38.100"
 Add-Content C:\Windows\system32\drivers\etc\hosts "`r`n$ip registry"
 
 Write-Host "Add Docker Registry"
-cp C:\ProgramData\docker\runDockerDaemon.cmd C:\ProgramData\docker\runDockerDaemon.cmd.bak
-cat C:\ProgramData\docker\runDockerDaemon.cmd.bak | %{$_ -replace '^dockerd -G docker -H npipe:// -H 0.0.0.0:2375$','dockerd -G docker -H npipe:// -H 0.0.0.0:2375 --insecure-registry registry:5000'} | Set-Content C:\ProgramData\docker\runDockerDaemon.cmd
+$path = (Get-ItemProperty -Path HKLM:SYSTEM\CurrentControlSet\Services\docker -Name ImagePath).ImagePath
+$path += ' --insecure-registry registry:5000'
+Set-ItemProperty -Path HKLM:SYSTEM\CurrentControlSet\Services\docker -Name ImagePath -Value $path -Force
 
 Write-Host "Restarting docker"
 Restart-Service docker
