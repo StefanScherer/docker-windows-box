a616892d8bd9b87099486310a98310b995890fca
Stefan Scherer <scherer_stefan@icloud.com>
2016-10-09 19:49:05 +0200

    Workaround: Run registry without volume mount point

--- swarm-demo/scripts/run-registry.ps1
+++ swarm-demo/scripts/run-registry.ps1
@@ -1,2 +1,4 @@
 mkdir -p C:\registry-v2
-docker run -d -p 5000:5000 -v "C:\registry-v2:C:\registry" stefanscherer/registry-windows:2.5.1
+# volume mount point does not work right now, see https://github.com/docker/distribution/issues/1732
+# docker run -d -p 5000:5000 -v "C:\registry-v2:C:\registry" stefanscherer/registry-windows:2.5.1
+docker run -d -p 5000:5000 stefanscherer/registry-windows:2.5.1
