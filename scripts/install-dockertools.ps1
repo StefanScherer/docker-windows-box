d8a39a7fc766b82efde1c4d4c8a16544edb8821d
Stefan Scherer <scherer_stefan@icloud.com>
2016-11-16 18:37:54 +0100

    Update docker-compose 1.9.0-rc4

--- scripts/install-dockertools.ps1
+++ scripts/install-dockertools.ps1
@@ -1,3 +1,3 @@
 # install docker tools
 choco install -y docker-machine -version 0.8.2
-choco install -y docker-compose -version 1.9.0-rc1 -pre
+choco install -y docker-compose -version 1.9.0-rc4 -pre
