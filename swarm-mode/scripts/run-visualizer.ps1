f88d6730ecf3af6cde22d3e4f3c8022ccb6dacd1
Stefan Scherer <scherer_stefan@icloud.com>
2016-11-24 00:22:31 +0100

    Retrieve IP address of docker host

--- swarm-mode/scripts/run-visualizer.ps1
+++ swarm-mode/scripts/run-visualizer.ps1
@@ -1,1 +1,4 @@
+$ip=(Get-NetIPAddress -AddressFamily IPv4 `
+   | Where-Object -FilterScript { $_.InterfaceAlias -Eq "vEthernet (HNS Internal NIC)" } `
+   ).IPAddress
 docker run -d -p 8080:8080 -e DOCKER_HOST=${ip}:2375 --name=visualizer stefanscherer/visualizer-windows
