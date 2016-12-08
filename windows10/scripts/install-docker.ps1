4de94f0b873f4bec821dbfe2753786bdcc48afa5
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-08 14:04:44 +0100

    Update Docker 1.13.0-rc3

--- windows10/scripts/install-docker.ps1
+++ windows10/scripts/install-docker.ps1
@@ -3,9 +3,9 @@ Set-ExecutionPolicy Bypass -scope Process
 
 New-Item -Type Directory -Path "$($env:ProgramFiles)\docker"
 $wc = New-Object net.webclient
-$wc.Downloadfile("https://test.docker.com/builds/Windows/x86_64/docker-1.13.0-rc2.zip", "$env:TEMP\docker-1.13.0-rc2.zip")
-Expand-Archive -Path $env:TEMP\docker-1.13.0-rc2.zip -DestinationPath $env:ProgramFiles -Force
-Remove-Item $env:TEMP\docker-1.13.0-rc2.zip
+$wc.Downloadfile("https://test.docker.com/builds/Windows/x86_64/docker-1.13.0-rc3.zip", "$env:TEMP\docker-1.13.0-rc3.zip")
+Expand-Archive -Path $env:TEMP\docker-1.13.0-rc3.zip -DestinationPath $env:ProgramFiles -Force
+Remove-Item $env:TEMP\docker-1.13.0-rc3.zip
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$($env:ProgramFiles)\docker", [EnvironmentVariableTarget]::Machine)
 $env:Path = $env:Path + ";$($env:ProgramFiles)\docker"
 . dockerd --register-service -H npipe:// -H 0.0.0.0:2375 -G docker
