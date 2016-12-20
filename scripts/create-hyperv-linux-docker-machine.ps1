1db3224b9790b3cfbc9781131220322bc73d120e
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-20 16:49:43 +0100

    Remove updating linux docker engine

--- scripts/create-hyperv-linux-docker-machine.ps1
+++ scripts/create-hyperv-linux-docker-machine.ps1
@@ -29,9 +29,3 @@ docker-machine create -d hyperv --hyperv-virtual-switch "docker" $name
 # workaround: https://github.com/docker/machine/issues/2267#issuecomment-156426874
 docker-machine start $name
 docker-machine regenerate-certs -f $name
-
-# windows docker client has different API version
-# workaround: update the docker engine in the Hyper-V Linux docker machine
-docker-machine ssh $name sudo /etc/init.d/docker stop
-docker-machine ssh $name sudo curl -o /usr/local/bin/docker https://master.dockerproject.org/linux/amd64/docker
-docker-machine ssh $name sudo /etc/init.d/docker start
