cc83b3677f87acc4830f1c0ad700c0a6a060d606
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-01 21:24:01 +0100

    Update Portainer 1.11.0

--- scripts/run-portainer.ps1
+++ scripts/run-portainer.ps1
@@ -5,10 +5,18 @@ $ip=(Get-NetIPAddress -AddressFamily IPv4 `
 docker kill portainer
 docker rm -vf portainer
 
+if (!(Test-Path C:\portainerdata)) {
+  mkdir C:\portainerdata
+}
+
 if (Test-Path $env:USERPROFILE\.docker\ca.pem) {
   docker run -d -p 8000:9000 `
-    -v $env:USERPROFILE\.docker:C:\certs --name portainer portainer/portainer:windows `
+    -v $env:USERPROFILE\.docker:C:\certs `
+    -v C:\portainerdata:C:\data `
+    --name portainer portainer/portainer:windows-1.11.0 `
     -H tcp://$($ip):2376 --tlsverify
 } else {
-  docker run -d -p 8000:9000 --name portainer portainer/portainer:windows -H tcp://$($ip):2375
+  docker run -d -p 8000:9000 `
+  -v C:\portainerdata:C:\data `
+  --name portainer portainer/portainer:windows-1.11.0 -H tcp://$($ip):2375
 }
