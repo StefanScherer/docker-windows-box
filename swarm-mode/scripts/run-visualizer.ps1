4b3138a8b43d31726c7dd62dae2a75866fc1d5bf
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-03 12:11:01 +0100

    Run visualizer with TLS certs if needed
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- swarm-mode/scripts/run-visualizer.ps1
+++ swarm-mode/scripts/run-visualizer.ps1
@@ -1,4 +1,13 @@
 $ip=(Get-NetIPAddress -AddressFamily IPv4 `
    | Where-Object -FilterScript { $_.InterfaceAlias -Eq "vEthernet (HNS Internal NIC)" } `
    ).IPAddress
-docker run -d -p 8080:8080 -e DOCKER_HOST=${ip}:2375 --name=visualizer stefanscherer/visualizer-windows
+
+if (Test-Path $env:USERPROFILE\.docker\ca.pem) {
+  docker run -d -p 8080:8080 `
+    -e DOCKER_HOST=${ip}:2376 -e DOCKER_TLS_VERIFY=1 `
+    -v "$env:USERPROFILE\.docker:C:\Users\ContainerAdministrator\.docker" `
+    --name=visualizer stefanscherer/visualizer-windows:allow-tls
+} else {
+  docker run -d -p 8080:8080 `
+    -e DOCKER_HOST=${ip}:2375 --name=visualizer stefanscherer/visualizer-windows:allow-tls
+}
