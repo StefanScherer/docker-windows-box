5eeac4f8e54a92c900d02797c5522a63021165ce
Oscar medina <me@sharepointoscar.com>
2017-03-23 14:06:31 -0700

    added additional basic packages for atom, to help be productive

--- scripts/install-atom.ps1
+++ scripts/install-atom.ps1
@@ -4,3 +4,7 @@ $env:PATH="$env:PATH;$env:LOCALAPPDATA\atom\bin"
 apm install language-powershell
 apm install language-batch
 apm install language-docker
+apm install franken-berry-ui
+apm install franken-berry-syntax
+apm install file-icons
+apm install platformio-ide-terminal
