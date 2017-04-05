a25a3f7e2f72fcd4bd0d9b3a5a8f29d5f80d08c1
Stefan Scherer <scherer_stefan@icloud.com>
2017-04-05 13:46:16 +0200

    Update download link

--- domain/scripts/generate-gMSA.ps1
+++ domain/scripts/generate-gMSA.ps1
@@ -7,7 +7,7 @@ New-ADServiceAccount -name WebApplication1 -DnsHostName Host1.contoso.local  -Se
 Install-AdServiceAccount WebApplication1
 Test-AdServiceAccount WebApplication1
 
-Invoke-WebRequest -OutFile CredentialSpec.psm1 -Uri https://github.com/Microsoft/Virtualization-Documentation/raw/804008c172b80a4f354a92cd4d12a4e23e1d4328/windows-server-container-tools/ServiceAccounts/CredentialSpec.psm1 -UseBasicParsing
+Invoke-WebRequest -OutFile CredentialSpec.psm1 -Uri https://raw.githubusercontent.com/Microsoft/Virtualization-Documentation/live/windows-server-container-tools/ServiceAccounts/CredentialSpec.psm1 -UseBasicParsing
 Import-Module ActiveDirectory
 Import-Module ./CredentialSpec.psm1
 New-CredentialSpec -Name WebApplication1 -AccountName WebApplication1
