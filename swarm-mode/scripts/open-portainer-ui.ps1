68ff3e77a617813473e3829236171cddf98c5081
Stefan Scherer <scherer_stefan@icloud.com>
2017-04-15 18:16:23 +0200

    Firewall exception no longer needed to access container ip

--- swarm-mode/scripts/open-portainer-ui.ps1
+++ swarm-mode/scripts/open-portainer-ui.ps1
@@ -1,4 +1,2 @@
 $ErrorActionPreference = 'Stop';
-& netsh advfirewall firewall add rule name="Portainer helper" dir=in action=allow protocol=TCP localport=9000
-
 start http://$(docker inspect -f '{{ .NetworkSettings.Networks.nat.IPAddress }}' $(docker ps -q --filter name=^/portainer)):9000
