c221bd67f6dd9cb9c953a097b6f932a501e2ca81
Stefan Scherer <scherer_stefan@icloud.com>
2017-07-17 21:19:21 +0200

    Install Docker 17.06.0-ce

--- windows10/scripts/install-docker.ps1
+++ windows10/scripts/install-docker.ps1
@@ -1,13 +1,12 @@
-# https://msdn.microsoft.com/en-us/virtualization/windowscontainers/quick_start/quick_start_windows_10
 Set-ExecutionPolicy Bypass -scope Process
-
 New-Item -Type Directory -Path "$($env:ProgramFiles)\docker"
-$wc = New-Object net.webclient
-$wc.Downloadfile("https://get.docker.com/builds/Windows/x86_64/docker-17.03.1-ce.zip", "$env:TEMP\docker-17.03.1-ce.zip")
-Expand-Archive -Path $env:TEMP\docker-17.03.1-ce.zip -DestinationPath $env:ProgramFiles -Force
-Remove-Item $env:TEMP\docker-17.03.1-ce.zip
+Write-Host "Downloading docker ..."
+wget -outfile $env:TEMP\docker.zip "https://download.docker.com/win/static/edge/x86_64/docker-17.06.0-ce.zip"
+Expand-Archive -Path $env:TEMP\docker.zip -DestinationPath $env:TEMP -Force
+copy $env:TEMP\docker\*.exe $env:ProgramFiles\docker
+Remove-Item $env:TEMP\docker.zip
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$($env:ProgramFiles)\docker", [EnvironmentVariableTarget]::Machine)
 $env:Path = $env:Path + ";$($env:ProgramFiles)\docker"
-. dockerd --register-service -H npipe:// -H 0.0.0.0:2375 -G docker --label os=windows
-
-Start-Service docker
+Write-Host "Registering docker service ..."
+. dockerd --register-service -H npipe:// -H 0.0.0.0:2375 -G docker
+Start-Service Docker
