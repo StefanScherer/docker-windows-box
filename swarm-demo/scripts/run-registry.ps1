3e8afa279424b43fe822abe3b34685ba4dc57c07
Stefan Scherer <scherer_stefan@icloud.com>
2017-07-18 17:21:50 +0200

    Update to stefanscherer/registry-windows:2.6.1

--- swarm-demo/scripts/run-registry.ps1
+++ swarm-demo/scripts/run-registry.ps1
@@ -1,4 +1,4 @@
 if (!(Test-Path C:\vagrant\registry-v2)) {
   mkdir -p C:\vagrant\registry-v2
 }
-docker run --restart=always -d -p 5000:5000 -v "C:\vagrant\registry-v2:C:\registry" stefanscherer/registry-windows:2.6.0
+docker run --restart=always -d -p 5000:5000 -v "C:\vagrant\registry-v2:C:\registry" stefanscherer/registry-windows:2.6.1
