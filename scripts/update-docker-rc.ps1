c57c079de46d81e488028c5f2a91026479a43c03
Stefan Scherer <scherer_stefan@icloud.com>
2017-09-14 09:39:02 +0200

    Do not change daemon.json to run in PWD

--- scripts/update-docker-rc.ps1
+++ scripts/update-docker-rc.ps1
@@ -9,14 +9,14 @@ Write-Host "Extracting docker-$version.zip"
 Expand-Archive -Path "$env:TEMP\docker-$version.zip" -DestinationPath $env:ProgramFiles -Force
 Remove-Item "$env:TEMP\docker-$version.zip"
 
-Write-Host "Activating experimental features"
-$daemonJson = "$env:ProgramData\docker\config\daemon.json"
-$config = @{}
-if (Test-Path $daemonJson) {
-  $config = (Get-Content $daemonJson) -join "`n" | ConvertFrom-Json
-}
-$config = $config | Add-Member(@{ experimental = $true }) -Force -PassThru
-$config | ConvertTo-Json | Set-Content $daemonJson -Encoding Ascii
+#Write-Host "Activating experimental features"
+#$daemonJson = "$env:ProgramData\docker\config\daemon.json"
+#$config = @{}
+#if (Test-Path $daemonJson) {
+#  $config = (Get-Content $daemonJson) -join "`n" | ConvertFrom-Json
+#}
+#$config = $config | Add-Member(@{ experimental = $true }) -Force -PassThru
+#$config | ConvertTo-Json | Set-Content $daemonJson -Encoding Ascii
 
 Write-Host "Starting docker service"
 Start-Service docker
