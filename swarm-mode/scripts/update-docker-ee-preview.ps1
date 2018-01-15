ae020527a527ff52e549ab6aa397ae33e549b876
Stefan Scherer <scherer_stefan@icloud.com>
2018-01-15 22:06:48 +0100

    Install 17.10.0-ee-preview-3

--- swarm-mode/scripts/update-docker-ee-preview.ps1
+++ swarm-mode/scripts/update-docker-ee-preview.ps1
@@ -3,6 +3,6 @@ Find-Package -ProviderName DockerProvider -AllVersions
 Uninstall-Package Docker
 # ignore error if CPU is not able to run Hyper-V, the preview package has a hard dependency
 $ErrorActionPreference = 'SilentlyContinue'
-Install-Package Docker -ProviderName DockerProvider -RequiredVersion preview -Force
+Install-Package Docker -ProviderName DockerProvider -RequiredVersion 17.10.0-ee-preview-3 -Force
 Start-Service docker
 docker version
