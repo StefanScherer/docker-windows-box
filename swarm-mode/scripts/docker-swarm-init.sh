0440fb639c0636be92338c25496220f2e874a432
Alexandr Timofeev <minimonium@outlook.com>
2018-11-10 02:25:14 +0300

    Fix docker service sed to expose Docker port 2375

--- swarm-mode/scripts/docker-swarm-init.sh
+++ swarm-mode/scripts/docker-swarm-init.sh
@@ -4,6 +4,6 @@ docker swarm init --listen-addr "$ip:2377" --advertise-addr "$ip:2377"
 
 echo "Warning: Do not insecure Docker port 2375 in production!"
 sudo systemctl stop docker
-sudo sed -i 's,-H fd://,-H tcp://0.0.0.0:2375 -H fd://,' /lib/systemd/system/docker.service
+sudo sed -i 's,dockerd,dockerd -H tcp://0.0.0.0:2375,' /lib/systemd/system/docker.service
 sudo systemctl daemon-reload
 sudo systemctl start docker
